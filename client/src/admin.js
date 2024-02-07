f1e1b86ebffc13ec58224fcbe1048936b1a9e432
xiomary <Xiomary331@gmail.com>
2024-02-06 23:08:33 -0500

    create exam button

---
+++ client/src/admin.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+export default function Admin() {
+  return (
+    <>
+    
+   <button class="btn btn-primary" type="submit">Create Exam</button>
+
+    </>
+
+  )
+}

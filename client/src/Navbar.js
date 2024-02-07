ea1cdf0c6e1c2da4824fd2c797928c31a8ae78eb
Tales Araujo Mota <talesamota27@gmail.com>
2024-02-07 18:04:39 -0500

    made the navBar into a functional component

---
+++ client/src/Navbar.js
@@ -0,0 +1,11 @@
+const Navbar = () => {
+    return ( 
+        <div class="navBar">
+            <a href ="#"> <i class="favicon"></i> </a>
+            <a href ="#">Exams</a>
+            <a href ="#">Admin</a>
+        </div>
+     );
+}
+ 
+export default Navbar;

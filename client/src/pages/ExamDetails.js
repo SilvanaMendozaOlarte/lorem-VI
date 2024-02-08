0d489e08b283450e3602b73cfd25d8642dd84339
DJRoche509 <davjroche@gmail.com>
2024-02-07 23:12:40 -0700

    Initial ExamDetails page commit

--- client/src/pages/ExamDetails.js
+++ client/src/pages/ExamDetails.js
@@ -1,6 +1,24 @@
+import React from "react";
+
 const ExamDetails = () => {
+    // Temporary Dummy data 
+    const patientExamData = {
+        patientInfo: {
+            
+        },
+        examInfo: {
+            
+        }
+    };
+
     return ( 
         <div className="examDetails">
+            < div className="patientInfo">
+                <h3>Patient Info</h3>
+            </div>
+            < div className="examInfo">
+                <h3>Exam Info</h3>
+            </div>
             
         </div>
      );

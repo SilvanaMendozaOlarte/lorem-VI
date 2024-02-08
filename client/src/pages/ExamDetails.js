4159311272dfc40f3fc178037aa6b1ce2b3dcc65
DJRoche509 <davjroche@gmail.com>
2024-02-07 23:30:37 -0700

    implement patientExamData Object with patient & exam Info (objects)

--- client/src/pages/ExamDetails.js
+++ client/src/pages/ExamDetails.js
@@ -4,10 +4,18 @@ const ExamDetails = () => {
     // Temporary Dummy data 
     const patientExamData = {
         patientInfo: {
-            
+            patientID: 'COVID-19-MA-123',
+            age: '45',
+            sex: 'M',
+            bmi: '24.5',
+            zipCode: '12345'
         },
         examInfo: {
-            
+            examID: '456',
+            imageUrl: 'https://www.nih.gov/sites/default/files/news-events/news-releases/2017/20170927-lung-mass.jpg', // Placeholder image URL 
+            date: '2024-02-06',
+            keyFindings: 'Patient key findings details here -- Test is Positive. Lungs ??? .',
+            brixiaScore: '4, 5, 6, 7'
         }
     };
 
@@ -15,9 +23,19 @@ const ExamDetails = () => {
         <div className="examDetails">
             < div className="patientInfo">
                 <h3>Patient Info</h3>
+                <p>Patient ID: {patientExamData.patientInfo.patientID}</p>
+                <p>Age: {patientExamData.patientInfo.age}</p>
+                <p>Sex: {patientExamData.patientInfo.sex}</p>
+                <p>BMI: {patientExamData.patientInfo.bmi}</p>
+                <p>Zip Code: {patientExamData.patientInfo.zipCode}</p>
             </div>
             < div className="examInfo">
                 <h3>Exam Info</h3>
+                <p>Exam ID: {patientExamData.examInfo.examID}</p>
+                <img src={patientExamData.examInfo.imageUrl} alt="X-Ray" />
+                <p>Date: {patientExamData.examInfo.date}</p>
+                <p>Key Findings: {patientExamData.examInfo.keyFindings}</p>
+                <p>Brixia Score: {patientExamData.examInfo.brixiaScore}</p>
             </div>
             
         </div>

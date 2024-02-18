9318617ce30e131cf9b906c2776e5af2c1269887
SilvanaMendozaOlarte <silvanamendozaolarte@gmail.com>
2024-02-18 17:31:08 -0500

    Moved search bar into its own file, now you can type into it

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 // import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
 import './App.css';
@@ -7,31 +7,8 @@ import ExamTable from './components/ExamTable.js';
 import { Box, Heading } from '@chakra-ui/react'
 import { BrowserRouter } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import { Search } from './components/Search.js';
 
-
-//search bar
-function Search(){
-  const[query,setquery] = useState("");
-  const getFilteredExams = (query,exams) => {
-    if(!query){
-      return exams;
-    }
-    return exams.filter(exams.name.includes(query))
-  }
-  const exams = [
-    { patientID: 1, examID: 1, keyFindings: 'A note.', brixiaScores: 1, age:1, sex:1, bmi:1, zip:1},
-    { patientID: 2, examID: 2, keyFindings: 'A note.', brixiaScores: 2, age:2, sex:1, bmi:1, zip:1},
-  ]
-  const filteredExams = getFilteredExams(query,exams)
-
-  return(
-    <div>
-      <label>Search: </label>
-      <input type="text" onChange= {e => setquery(e.target.value)} />
-    </div>
-    );
-    
-  }
 
 function App() {
   const { response } = useApi();

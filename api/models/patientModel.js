08cb67e29c64215caae7abc0a2a25f83b9f73fd3
githendumukiri@gmail.com <githendumukiri@gmail.com>
2024-02-23 02:26:43 -0500

    testing tools

--- api/models/patientModel.js
+++ api/models/patientModel.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Define schema and model for patients
 const patientSchema = new mongoose.Schema({
-    patientId: String,
-    age: Number,
-    sex: String,
-    zipCode: String,
-    bmi: Number
+  patientId: String,
+  age: Number,
+  sex: String,
+  zipCode: String,
+  bmi: Number,
 });
-  
-module.exports = mongoose.model('Patient', patientSchema);
+
+module.exports = mongoose.model("Patient", patientSchema);
